d8489f4dda207f7ddf1d79638320433f544f6084
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-04 22:00:52 -0300

    feat: Adicionando o Card de posts

---
+++ react-app/src/app/components/Card/Card.jsx
@@ -0,0 +1,54 @@
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button, IconButton,
+} from "@material-tailwind/react";
+
+export function CardDefault() {
+
+    const is = true
+    return (
+        <div className="w-full h-full flex justify-center">
+            <Card className="mt-9 w-1/3 b-">
+                <CardHeader color="blue-gray" className="relative h-56">
+                    <img
+                        src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                        alt="card-image"
+                    />
+                </CardHeader>
+                <CardBody>
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                        UI/UX Review Check
+                    </Typography>
+                    <Typography>
+                        The place is close to Barceloneta Beach and bus stop just 2 min by
+                        walk and near to &quot;Naviglio&quot; where you can enjoy the main
+                        night life in Barcelona.
+                    </Typography>
+                </CardBody>
+                <CardFooter className="pt-0">
+                    <IconButton className="m-2">
+                        {is ?
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                            </svg>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        }
+                    </IconButton>
+                    <IconButton>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path fillRule="evenodd" d="M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z" clipRule="evenodd" />
+                        </svg>
+                    </IconButton>
+                </CardFooter>
+            </Card>
+        </div>
+
+    );
+}
