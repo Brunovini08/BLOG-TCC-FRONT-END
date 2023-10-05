55e49146c7147585435b107aa26be5b39a69a4e1
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-04 22:02:56 -0300

    feat: Adicionando page Home

---
+++ react-app/src/app/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/Navbar/NavBar.jsx";
+import {CardDefault} from "../../components/Card/Card.jsx";
+function Home() {
+
+  return (
+    <div className="">
+         <NavBar />
+         <CardDefault />
+    </div>
+  )
+}
+
+export default Home
