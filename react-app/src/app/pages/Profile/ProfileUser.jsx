62ed7a39320570d8f453e6520a1cfb2c6540dc09
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-04 22:00:08 -0300

    feat: Adicionando Profile do User

---
+++ react-app/src/app/pages/Profile/ProfileUser.jsx
@@ -0,0 +1,12 @@
+
+import {User} from "../../components/User/User.jsx";
+
+
+export default function ProfileUser() {
+    console.log("oi")
+    return(
+        <div>
+            <User />
+        </div>
+    )
+}
