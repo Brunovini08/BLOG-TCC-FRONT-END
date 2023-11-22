20cdd4ae404d2cc510dcb1aa8ef50a083e401838
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-11-21 21:22:15 -0300

    fix: modificando a service de rotas do projeto

--- react-app/src/routes/index.jsx
+++ react-app/src/routes/index.jsx
@@ -1,51 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { PrivateRoutes } from "./privateRoutes";
-import Login from "../app/pages/Login/Login.jsx";
+import {Login} from "../app/pages/Login/Login.jsx";
 import Register from "../app/pages/Register/Register.jsx";
-import CreatePost from "../app/pages/CreatePost/CreatePost.jsx";
+import {CreatePost} from "../app/pages/CreatePost/CreatePost.jsx";
 import ProfileUser from "../app/pages/Profile/ProfileUser.jsx";
 import Home from "../app/pages/Home/Home.jsx";
-import Support from "../app/pages/Support/Support.jsx";
+import {ForgetPass} from "../app/pages/ForgetPass/ForgetPass.jsx";
+import {Post} from "../app/pages/Post/Post.jsx";
+import {ProfileOthersUsers} from "../app/pages/ProfileOthersUsers/ProfileOthersUsers.jsx";
 import {Settings} from "../app/pages/Settings/Settings.jsx";
-import {MyPosts} from "../app/pages/MyPosts/MyPosts.jsx";
+import {EditPost} from "../app/pages/EditPost/EditPost.jsx";
+import {ForgetPassEmail} from "../app/pages/ForgetPass/ForgetPassEmail.jsx";
 
 export const AppRouter = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/">
-                    <Route path="register" element={<Register />}/>
-                </Route>
-                <Route path="/">
-                    <Route path="support" element={<Support />}/>
-                </Route>
-                <Route path="/">
-                    <Route path="login" element={<Login />} />
-                </Route>
-                <Route path="/">
-                    <Route path="home" element={<Home />}/>
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="createpost" element={<CreatePost />} />
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="profile" element={<ProfileUser />}/>
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="createpost" element={<CreatePost />}/>
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="myposts" element={<MyPosts />}/>
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="settings" element={<Settings />}/>
-                </Route>
-                <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="signout" element={<Home />}/>
-                </Route>
-            </Routes>
-        </Router>
+       <BrowserRouter>
+           <Routes>
+               <Route path="/" element={<Home />}/>
+               <Route path="/login" element={<Login />}/>
+               <Route path="/register" element={<Register />}/>
+               <Route path="/createpost/:id" element={<PrivateRoutes><CreatePost /></PrivateRoutes>}/>
+               <Route path="/resetpassword/:id/:token" element={<ForgetPass />}/>
+               <Route path="/profile" element={<PrivateRoutes><ProfileUser /></PrivateRoutes>}/>
+               <Route path="/settings/:id" element={<PrivateRoutes><Settings /></PrivateRoutes>}/>
+               <Route path="/post?/:id/:opencomment?" element={<Post />}  />
+               <Route path="/profile/:id" element={<ProfileUser />}></Route>
+               <Route path="/profile/otheruser/:id" element={<ProfileOthersUsers />}></Route>
+               <Route path="/editpost/:id" element={<PrivateRoutes><EditPost /></PrivateRoutes>}></Route>
+               <Route path="/forgetpasswordemail" element={<ForgetPassEmail />}/>
+           </Routes>
+       </BrowserRouter>
     );
 };
 
