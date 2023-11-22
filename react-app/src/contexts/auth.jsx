6584f31e5fcd5ced114a9c08c890eb3e177d3f88
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-11-21 21:23:27 -0300

    feat: adicionando funcionalidades na service de autenticação do user

--- react-app/src/contexts/auth.jsx
+++ react-app/src/contexts/auth.jsx
@@ -1,6 +1,6 @@
 import {createContext, useEffect, useState} from "react";
-import {Navigate} from "react-router-dom";
-import authService from "../services/UserService.js";
+import {Navigate, useNavigate} from "react-router-dom";
+import authService from "../services/UserService/UserService.js";
 import axios from "axios";
 
 export const AuthContext = createContext()
@@ -27,39 +27,25 @@ export const AuthProvider = ({children}) => {
             const response = await authService.login({email, password})
 
             if (response.data.error) {
-                alert(response.data.error);
+                alert(response.data.error)
             } else {
-                console.log(response.data)
                 setUser(response.data);
-                axios.defaults.headers.common[
-                    "Authorization"
-                    ] = `Bearer ${response.data.token}`;
-
-                localStorage.setItem("@Auth:user", JSON.stringify());
+                localStorage.setItem("@Auth:user", JSON.stringify(response.data));
                 localStorage.setItem("@Auth:token", response.data.token);
             }
         } catch (error) {
-            console.log(error);
+            const { data } = error.response;
+            return data;
         }
     };
 
     const signUp = async ({name, email, password}) => {
         try {
             const response = await authService.register({name, email, password})
-
             if (response.data.error) {
                 alert(response.data.error)
-            } else{
-                console.log(response.data)
-                setUser(response.data)
-                axios.defaults.headers.common[
-                    "Authorization"
-                    ] = `Bearer ${response.data.token}`;
-
-
-                localStorage.setItem("@Auth:user", JSON.stringify({name: response.data.name, email: response.data.email}))
-                localStorage.setItem("@Auth:token", response.data.token)
             }
+            setUser(response.data)
         }catch (error) {
             console.log(error)
         }
@@ -68,8 +54,47 @@ export const AuthProvider = ({children}) => {
     const signOut = () => {
         localStorage.clear();
         setUser(null);
-        return <Navigate to="/" />;
+        const navigate = useNavigate()
+        navigate("/")
     };
+
+    const modUser = async ({name, biografy, localization, image}) => {
+        try {
+            const response = await authService.modUser({name, biografy, localization, image}).then((response) => {
+                localStorage.setItem("@Auth:user", JSON.stringify(response.data))
+            }).catch((error) => {
+                const {data} = error.response
+                return data
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const forgetPassword = async (data, id, token) => {
+        try {
+            const response = await authService.forgetPassword(data, id, token)
+            if (response.data.error) {
+                console.log(response.data.error)
+            }
+            return response.data
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    const forgetPassEmail = async (data) => {
+        try {
+            const response = await authService.forgetPassEmail(data)
+            if (response.data.error) {
+                console.log(response.data.error)
+            }
+            return response.data
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
 
     return(
         <AuthContext.Provider value={{
@@ -77,6 +102,9 @@ export const AuthProvider = ({children}) => {
             signIn,
             signOut,
             signUp,
+            modUser,
+            forgetPassword,
+            forgetPassEmail,
             signed: !!user
         }}>
             {children}
